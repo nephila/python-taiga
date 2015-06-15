80e89b81c9947a3a3816ec577bf2889c9a0719a6
Andrea Stagi <stagi.andrea@gmail.com>
2015-06-15 08:06:37 +0200

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name="python-taiga",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     include_package_data=True,
     description="Taiga python API",
--- taiga/__init__.py
+++ taiga/__init__.py
@@ -5,7 +5,7 @@
 """
 Taiga Python API library
 """
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Nephila'
 __license__ = 'MIT'
 
