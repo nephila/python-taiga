c7e22fe578f76c91c22966459ba05dd9245bc32a
Andrea Stagi <stagi.andrea@gmail.com>
2015-07-14 14:35:35 +0200

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name="python-taiga",
-    version="0.3.1",
+    version="0.4.0",
     packages=find_packages(),
     include_package_data=True,
     description="Taiga python API",
