1ebfd515e92e5617c3c6ee5b85879541405ca4d3
Andrea Stagi <stagi.andrea@gmail.com>
2015-11-19 11:22:17 +0100

    Bump version number to 0.8.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name="python-taiga",
-    version="0.7.0",
+    version="0.8.0",
     packages=find_packages(),
     include_package_data=True,
     description="Taiga python API",
