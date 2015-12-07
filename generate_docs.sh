9c11c894983299ad8987f8afce0b7b0fd526b010
Andrea Stagi <stagi.andrea@gmail.com>
2015-12-07 16:44:14 +0100

    Update script for doc generation

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,5 +1,14 @@
-rm -rf docs/build/
+export LC_ALL=en_US.UTF-8
+
+rm -rf docs/build/.doctrees docs/build/_sources docs/build/static docs/build/.buildinfo
+rm -rf docs/build/*.*
 
 pip install . --upgrade
 
 sphinx-build -b html docs/ docs/build
+
+mv docs/build/_static docs/build/static
+
+sed -i '' 's/_static/static/g' docs/build/index.html
+
+pip uninstall python-taiga --yes
