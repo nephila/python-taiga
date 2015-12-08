c2667f99f16c74b35439314d183d422a1c5a04cf
Andrea Stagi <stagi.andrea@gmail.com>
2015-12-08 03:20:45 +0100

    Fix "sources" folder path in generated doc

--- generate_docs.sh
+++ generate_docs.sh
@@ -8,8 +8,10 @@ pip install . --upgrade
 sphinx-build -b html docs/ docs/build
 
 mv docs/build/_static docs/build/static
+mv docs/build/_sources docs/build/sources
 
 sed -i '' 's/_static/static/g' docs/build/index.html
+sed -i '' 's/_sources/sources/g' docs/build/index.html
 sed -i '' 's/_static/static/g' docs/build/genindex.html
 sed -i '' 's/_static/static/g' docs/build/py-modindex.html
 sed -i '' 's/_static/static/g' docs/build/search.html
