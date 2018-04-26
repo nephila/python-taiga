5b0489002ec7ce371c36d1d53aae0255ff265a28
Mario Orlandi <morlandi@brainstorm.it>
2018-04-26 08:33:52 +0200

    Automatically import Epic and Epics from models

--- taiga/models/__init__.py
+++ taiga/models/__init__.py
@@ -1,7 +1,7 @@
 from .models import (
-    History, Issue, IssueAttachment, IssueAttachments, IssueAttribute, IssueAttributes, Issues, IssueStatus,
-    IssueStatuses, IssueType, IssueTypes, Membership, Memberships, Milestone, Milestones, Point, Points, Priorities,
-    Priority, Project, Projects, Role, Roles, Severities, Severity, Task, TaskAttachment, TaskAttachments,
+    Epic, Epics, History, Issue, IssueAttachment, IssueAttachments, IssueAttribute, IssueAttributes, Issues,
+    IssueStatus, IssueStatuses, IssueType, IssueTypes, Membership, Memberships, Milestone, Milestones, Point, Points,
+    Priorities, Priority, Project, Projects, Role, Roles, Severities, Severity, Task, TaskAttachment, TaskAttachments,
     TaskAttribute, TaskAttributes, Tasks, TaskStatus, TaskStatuses, User, Users, UserStories, UserStory,
     UserStoryAttachment, UserStoryAttachments, UserStoryAttribute, UserStoryAttributes, UserStoryStatus,
     UserStoryStatuses, Webhook, Webhooks, WikiLink, WikiLinks, WikiPage, WikiPages,
@@ -18,5 +18,5 @@ __all__ = [
     'History', 'IssueAttribute', 'IssueAttributes', 'TaskAttribute',
     'TaskAttributes', 'UserStoryAttribute', 'UserStoryAttributes',
     'Membership', 'Memberships', 'Role', 'Roles', 'IssueType', 'IssueTypes',
-    'Webhook', 'Webhooks'
+    'Webhook', 'Webhooks', 'Epic', 'Epics',
 ]
