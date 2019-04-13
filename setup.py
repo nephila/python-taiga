9228143d2abe58d578cf196dfbf4ce14ed3cd753
Mario Orlandi <morlandi@brainstorm.it>
2019-04-13 11:25:24 +0200

    Test with Python 3.7

--- setup.py
+++ setup.py
@@ -39,5 +39,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
